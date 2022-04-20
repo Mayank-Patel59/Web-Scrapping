9a7113f3c67a1105f780950feccfe587c5fbcf1e
Mayank-Patel59 <52083401+Mayank-Patel59@users.noreply.github.com>
2022-04-20 10:55:43 +0530

    Update Myfile.py

--- Myfile.py
+++ Myfile.py
@@ -3,25 +3,29 @@ import  csv
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+#Taking Input from the User (For Moie search)
 print('Enter Movie Name that you want to Search: ');
 input1 = input()
 
-quote_page = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+quote_page = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'   #IMDB website from where data is coming
 
 page = urllib.request.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
-rating_box = soup.findAll('td', attrs={'class': 'ratingColumn imdbRating'})
+rating_box = soup.findAll('td', attrs={'class': 'ratingColumn imdbRating'})    
 name_box = soup.findAll('td', attrs={'class': 'titleColumn'})
 
+
+#Running loop for search & find the movie searched by user 
 for val in range (0, 249):
     name = name_box[val].text.strip()
     #print(name);
 
     rating = rating_box[val].text.strip()
     #print(rating);
-
+    
+    #Printing the movie details searched by user
     if(input1 in name):
         print('Rating For '+ name + ' is: ' +rating)
 
